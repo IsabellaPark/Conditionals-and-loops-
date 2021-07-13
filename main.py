6205e60f5e136a48683c82ae8acca374a8a3f3d0
ParkBella <87280152+IsabellaPark@users.noreply.github.com>
2021-07-13 22:02:24 +0000

    loops

--- main.py
+++ main.py
@@ -1,6 +1,51 @@
 # warm up
 # get users first and last name
 # print the name reverse order 
-name1 = input("What is your first name? ")
-name2 = input("What is your last name? ")
-print("Your name is: " + name2 + name1)
+  # name1 = input("What is your first name? ")
+  # name2 = input("What is your last name? ")
+  # print("Your name is: " + name2, name1)
+#--------------------------------------------------------
+
+# Loops
+# while loops
+
+# while loop takes in a boolean experetion (T/F)
+# Boolean operaters:
+# Comparison operaters 
+# let 3 = x
+# x < 5 (less than 5)
+# x > 5 (greater than 5)
+# let x = 5
+# x <= 5 (less than or equall to 5)
+# x >= 5 (greater than or equall to 5)
+# x == 5 (equall to 5)
+# x != 5 (not equall to 5)
+
+# Logical operaters
+#and 
+#or
+#not
+
+# A while loop will only run if the boolean experetion is true
+# counter = 0
+# while(counter <= 5):
+#   print(counter)
+#   counter = counter + 1
+
+# for loops
+# counter = 1
+# for counter in range(6):
+#   print(counter)
+
+# counter = 0
+# for counter in range(0, 5):
+#   print(counter + 1)
+
+#list od numbers 
+numbers = [1, 2, 3, 4, 5]
+sum = 0
+# get the sum of all numbers
+for numbers in range(1, 6):
+  sum = sum + numbers 
+  print(sum)
+
