0e3051ba3755bf808fbef291a7df1f1e799b7c23
ParkBella <87280152+IsabellaPark@users.noreply.github.com>
2021-07-13 21:07:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# warm up
+# get users first and last name
+# print the name reverse order 
+name1 = input("What is your first name? ")
+name2 = input("What is your last name? ")
+print("Your name is: " + name2 + name1)
